8fe24a8ce487e7534f700587957afbff54d710d7
Smiley <jcsmileyjr@gmail.com>
2023-01-14 14:26:23 -0600

    Initial TODO planning

--- react-tip-calculator/src/App.js
+++ react-tip-calculator/src/App.js
@@ -1,6 +1,19 @@
 
 import './App.css';
 
+/**
+ * React app that displays a tip Calculator. 
+ * Todo:
+ * 1. Setup basic UI architecture with initial responsive design
+ * 2. Add intial rough UI layout with dummy content with no styling
+ * 3. Add initial styling to match frontend design
+ * 4. Update responsive design for mobile and desktop
+ * 5. Refactor to add state for bill amount and number of people
+ * 6. Refactor to add functionality to update tip amount and total per person
+ * 7. Refactor to change the above based on tip percentage
+ * 8. Refactor to add accessibilty features
+ * @returns 
+ */
 function App() {
   return (
     <div className="App">
